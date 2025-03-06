c41311fd405688348167da0bf82dd7d4d2da9a9a
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2025-03-06 23:01:33 +0900

    Data Validation Completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,9 +14,9 @@ FILE_NAME: str = "NetworkData.csv"
 TRAIN_FILE_NAME: str = "train.csv" 
 TEST_FILE_NAME: str = "test.csv"
 
-
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 """
-Data Ingestion realted constant starts with DATA_INGESTION VAR NAME 
+Data Ingestion related constant starts with DATA_INGESTION VAR NAME 
 """
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "YANGHO"
@@ -24,3 +24,11 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation related constants start with DATA_VALIDATION VAR NAME"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
